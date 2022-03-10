0159cff7ddec62585cfa5dbf28f70721120536a1
kinshukphalke <phalkekinshuk@gmail.com>
2022-03-10 17:17:29 -0500

    Merged Parser Branch Changes

--- tools/GenerateAST.py
+++ tools/GenerateAST.py
@@ -3,16 +3,37 @@ import sys
 tab = "    " # Tab is four spaces
 base_desc = {
     "Expr": {
+        "Assign" : [["Token.Token", "name"], ["Expr", "value"]],
         "Chain": [["Expr", "left"], ["Expr", "right"]],
-        "Unary": [["Scanner.Token", "operator"], ["Expr", "right"]],
-        "Binary": [["Expr", "left"], ["Scanner.Token", "operator"], ["Expr", "right"]],
+        "Unary": [["Token.Token", "operator"], ["Expr", "right"]],
+        "Binary": [["Expr", "left"], ["Token.Token", "operator"], ["Expr", "right"]],
+        "Logical": [["Expr", "left"], ["Token.Token", "operator"], ["Expr", "right"]],
         "Grouping" : [["Expr", "expression"]],
-        "Literal" : [["object", "value"]]
+        "Literal" : [["object", "value"]],
+        "Variable" : [["Token.Token", "name"]],
+        "Variable" : [["Token.Token", "keyword"]],
+        "Call" : [["Expr","callee"], ["Token.Token", "parent"], ["list","arguments"]]
+    },
+    "Stmt": {
+        "Expression" : [["Expr", "expression"]],
+        "Function" : [["Token.Token", "name"], ["list", "params"],["list", "body"]],
+        "Expression" : [["Expr", "expression"]],
+        "If": [["Expr", "condition"], ["Stmt", "thenBranch"],["Stmt", "elseBranch"]],
+        "Print" : [["Expr","expression"]],
+        "Return" : [["Token.Token","keyword"], ["Expr","value"]],
+        "Var" : [["Token.Token", "name"], ["Expr", "initializer"]],
+        "While" : [["Expr","condition"], ["Stmt", "body"]]
     }
 }
 
 def defineAst(file, baseName, types):
-    file.write("import Scanner\n\n\n")
+    file.write("import Scanner\nimport Token\n\n")
+    file.writelines(["class " + baseName + ":\n", tab + "pass\n\n"])
+    for ExprType, expr in types.items():
+        defineType(file, baseName, ExprType, expr)
+
+def defineStmt(file, baseName, types):
+    file.write("import Scanner\nimport Token\nimport Expr\n\n")
     file.writelines(["class " + baseName + ":\n", tab + "pass\n\n"])
     for ExprType, expr in types.items():
         defineType(file, baseName, ExprType, expr)
@@ -34,3 +55,8 @@ def defineType(file, baseName, className, fields):
 path = "../lox/Expr.py"
 with open(path, "w+") as file:
     defineAst(file, "Expr", base_desc["Expr"])
+
+path = "../lox/Stmt.py"
+with open(path, "w+") as file:
+    defineStmt(file, "Stmt", base_desc["Stmt"])
+    # defineAst(file, "Expr", base_desc["Stmt"])
