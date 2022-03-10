92be00b1244d2e5712e89263525e47c2aba6e991
kinshukphalke <phalkekinshuk@gmail.com>
2022-03-10 17:08:25 -0500

    Added primary Function

--- lox/Parser.py
+++ lox/Parser.py
@@ -1,23 +1,44 @@
 import Expr
 import Scanner
 import TokenType
+import Expr
+import Stmt
 
 class Parser:
-
     def __init__(self, tokenList):
         self.tokenList = tokenList
 
     def advance(self):
         if not self.isAtEnd():
-              self.current += 1
+            self.current += 1
         return self.previous()
 
     def match(self, *tokenTypes):
         for token in tokenTypes:
-              if self.check(token):
-                    self.advance()
-                    return True
+            if self.check(token):
+                self.advance()
+                return True
         return False
+
+    def unary(self):
+        if self.match(TokenType.TokenType.BANG, TokenType.TokenType.MINUS):
+            operator = self.previous()
+            right = self.unary()
+            return Expr.Unary(operator, right)
+        return self.primary()
+
+    def primary(self):
+        if self.match(TokenType.TokenType.FALSE):
+            return Expr.Literal(False)
+        elif self.match(TokenType.TokenType.TRUE):
+            return Expr.Literal(True)
+        elif self.match(TokenType.TokenType.NIL):
+            return Expr.Literal(None)
+        elif self.match(TokenType.TokenType.NUMBER, TokenType.TokenType.STRING):
+            return Expr.Literal(self.previous().literal)
+        elif self.match(TokenType.TokenType.LEFT_PAREN):
+            pass
+            #to implement
 
     def check(self, tokenType):
         if self.isAtEnd():
