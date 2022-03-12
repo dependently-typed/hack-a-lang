cbcf3220ac215cccfbd72544d12e516a38fa7ba5
Brenton Jackson <bjavonjackson@gmail.com>
2022-03-12 15:06:20 -0500

    added semicolon syntax check

--- soln/Lox.py
+++ soln/Lox.py
@@ -6,36 +6,40 @@ import Scanner
 import Token
 from TokenType import TokenType
 
+
 class Lox:
     def __init__(self):
         self.interpreter = Interpreter.Interpreter()
         self.hadError = False
         self.hadRuntimeError = False
 
-
     def runFile(self, path: str) -> None:
         source = None
         try:
             with open(path, 'r') as f:
-                source = f.read() # Probably not safe
+                source = f.read()  # Probably not safe
         except FileNotFoundError:
             sys.exit(f"FileNotFoundError: No such file or directory: {path}")
         self.run(source)
-        if self.hadError: exit(65)
-        if self.hadRuntimeError: exit(70)
-
+        if self.hadError:
+            exit(65)
+        if self.hadRuntimeError:
+            exit(70)
 
     def runPrompt(self) -> None:
-        pass # maybe in the future
-
+        pass  # maybe in the future
 
     def run(self, source: str) -> None:
         scanner = Scanner.Scanner(source)
         tokens = scanner.scanTokens()
         parser = Parser.Parser(tokens)
         statements = parser.parse()
-        if self.hadError: return
-        if self.hadRuntimeError: return
+        if statements == [None]:
+            self.missingSemicolonError(tokens)
+        if self.hadError:
+            return
+        if self.hadRuntimeError:
+            return
         self.interpreter.interpret(statements)
 
     def lineError(self, line: int, message: str) -> None:
@@ -46,12 +50,16 @@ class Lox:
         self.hadError = True
 
     def tokenError(self, token: Token.Token, message: str) -> None:
-        if token.type == TokenType.TokenType.EOF:
+        if token.type == TokenType.EOF:
             self.report(token.line, " at end", message)
         else:
-            self.report(token.line, " at '" + token.lexeme + "'" + message)
+            self.report(token.line, " at '" + token.lexeme + "'", message)
 
     def runtimeError(self, error: RuntimeError) -> None:
         print(f"{str(error)}\n[line {error.token.line}]")
         self.hadRuntimeError = True
 
+    def missingSemicolonError(self, tokens: list[Token.Token]) -> None:
+        lastToken = tokens[-2]
+        if (lastToken.type == TokenType.STRING):
+            return self.tokenError(lastToken, "Missing semicolon :/")
